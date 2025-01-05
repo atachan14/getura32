217129d1fa43115de3e2cda9971dad5e8e9a94ec
WIN-1Q8DR817JKE\user <aklius14pg@gmail.com>
2025-01-06 04:55:59 +0900

    触手の同期処理の途中　Clientで表示されない。

--- Assets/Script/lovi/TentacleController.cs
+++ Assets/Script/lovi/TentacleController.cs
@@ -1,26 +1,48 @@
+using System.Globalization;
 using Unity.Netcode;
-using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine;
 
 public class TentacleController : NetworkBehaviour
 {
     public GameObject inactiveTentacle;
     public GameObject activeTentacle;
+
     private GameObject targetPlayer;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    // 触手の状態を同期するためのNetworkVariable
+    private NetworkVariable<Vector3> tentaclePosition = new NetworkVariable<Vector3>();
+    private NetworkVariable<Quaternion> tentacleRotation = new NetworkVariable<Quaternion>();
+    private NetworkVariable<float> tentacleScaleY = new NetworkVariable<float>();
+
     void Start()
     {
         DeactivateTentacle();
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (IsOwner)
         {
-            if (Input.GetMouseButtonDown(0)) ActivateTentacle();
-            if (targetPlayer != null) KeepTentacle();
+            if (Input.GetMouseButtonDown(0))
+            {
+                ActivateTentacleServerRpc();
+            }
+
+            if (targetPlayer != null)
+            {
+                KeepTentacle();
+            }
         }
+
+        // 触手の同期情報を反映
+        SyncTentacle();
+    }
+
+    [ServerRpc]
+    public void ActivateTentacleServerRpc()
+    {
+        ActivateTentacle();
     }
 
     public void KeepTentacle()
@@ -30,43 +52,51 @@ public class TentacleController : NetworkBehaviour
 
         Vector3 direction = targetPosition - activeTentacle.transform.position;
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-        activeTentacle.transform.rotation = Quaternion.Euler(0, 0, angle - 90);
 
-        // 距離を正確に計算して画像を引き延ばす
+        // 触手の回転・スケール計算
+        activeTentacle.transform.rotation = Quaternion.Euler(0, 0, angle - 90);
         float distance = direction.magnitude;
         float spriteHeight = activeTentacle.GetComponent<SpriteRenderer>().sprite.bounds.size.y;
         activeTentacle.transform.localScale = new Vector3(1, distance / spriteHeight, 1);
+
+        // サーバーに同期データを送信
+        UpdateTentacleDataServerRpc(activeTentacle.transform.position, activeTentacle.transform.rotation, activeTentacle.transform.localScale.y);
+    }
+
+    [ServerRpc]
+    public void UpdateTentacleDataServerRpc(Vector3 position, Quaternion rotation, float scaleY)
+    {
+        // 計算したデータのみを同期
+        tentaclePosition.Value = position;
+        tentacleRotation.Value = rotation;
+        tentacleScaleY.Value = scaleY;
+    }
+
+    public void SyncTentacle()
+    {
+        // NetworkVariableの値をUIに反映
+        activeTentacle.transform.position = tentaclePosition.Value;
+        activeTentacle.transform.rotation = tentacleRotation.Value;
+        activeTentacle.transform.localScale = new Vector3(1, tentacleScaleY.Value, 1);
+    }
+
+    [ServerRpc]
+    public void DeactivateTentacleServerRpc()
+    {
+        DeactivateTentacle();
     }
 
     public void ActivateTentacle()
     {
-        // 非アクティブ触手を隠す
+        activeTentacle.GetComponent<RectTransform>().localPosition = new Vector3(0, 0, 5);
         inactiveTentacle.SetActive(false);
-
-        // アクティブ触手を表示
         activeTentacle.SetActive(true);
 
-        // マウス位置でのRaycastを実行
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction);
 
-        if (hit.collider != null&& hit.collider.CompareTag("CharacterClick"))
+        if (hit.collider != null && hit.collider.CompareTag("CharacterClick"))
         {
-            // 触手のターゲット位置を取得 (ヒット地点)
-            Vector3 targetPosition = hit.point;
-            targetPosition.z = activeTentacle.transform.position.z; // Z座標を揃える
-
-            // 対象の方向を計算
-            Vector3 direction = targetPosition - activeTentacle.transform.position;
-            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-            activeTentacle.transform.rotation = Quaternion.Euler(0, 0, angle); // 必要なら補正
-         
-
-            // 距離を正確に計算して画像を引き延ばす
-            float distance = direction.magnitude;
-            float spriteHeight = activeTentacle.GetComponent<SpriteRenderer>().sprite.bounds.size.y;
-            activeTentacle.transform.localScale = new Vector3(1, distance / spriteHeight, 1);
-            
             targetPlayer = hit.collider.gameObject;
         }
         else if (EventSystem.current.IsPointerOverGameObject())
@@ -75,16 +105,9 @@ public class TentacleController : NetworkBehaviour
         }
         else
         {
-            // マウスがヒットしなければ触手を非表示
             DeactivateTentacle();
-            Debug.Log("No hit detected. Tentacle deactivated.");
         }
     }
-
-
-
-
-
 
     public void DeactivateTentacle()
     {
