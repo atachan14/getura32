9c17aedd7def35c9238a161c51b8094beb53db60
DESKTOP-2DNVOOR\240925PM <aklius14pg@gmail.cm>
2025-02-01 20:48:08 +0900

    png追加

--- Assets/Script/TuraaCustamize/BallSet.cs
+++ Assets/Script/TuraaCustamize/BallSet.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 public class BallSet : MonoBehaviour
 {
     public static BallSet C;
-    [SerializeField] List<List<Sprite>> spritesList=new();
+    [SerializeField] List<List<Sprite>> spritesList;
+    [SerializeField] List<Sprite> sp0 = new();
+    [SerializeField] List<Sprite> sp1 = new();
+    [SerializeField] List<Sprite> sp2 = new();
 
     private void Awake()
     {
+        spritesList = new List<List<Sprite>> {
+            sp0 ,
+            sp1 ,
+            sp2
+        };
         C = this;
     }
     public List<Sprite> getSprites(int index)
--- Assets/Script/TuraaCustamize/EyeSet.cs
+++ Assets/Script/TuraaCustamize/EyeSet.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 public class EyeSet : MonoBehaviour
 {
     public static EyeSet C;
-    [SerializeField] List<List<Sprite>> spritesList = new();
+    [SerializeField] List<List<Sprite>> spritesList;
+    [SerializeField] List<Sprite> sp0 = new();
+    [SerializeField] List<Sprite> sp1 = new();
+    [SerializeField] List<Sprite> sp2 = new();
 
     private void Awake()
     {
+        spritesList = new List<List<Sprite>> {
+            sp0 ,
+            sp1 ,
+            sp2
+        };
         C = this;
     }
     public List<Sprite> getSprites(int index)
